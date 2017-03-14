86b83911ab50ec96e1f5479619a0224a1ad3a24c
sutaiyun <sutaiyun@longcheer.net>
2017-03-14 14:29:36 +0800

    重构  city demo的mysql 访问位置。 http://localhost:8081/cities  访问成功。
    注意，  SqlSessionFactoryBuilder  全系统只需要一个，  SqlSessionFactory 也是很重的对象。
    实际使用时，还需重构 这些对象。

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,11 +1,5 @@
 package com.example;
 
-import com.example.datasource.dao.City;
-import com.example.datasource.mapper.CityMapper;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,10 +7,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.util.Date;
 
 /**
@@ -43,11 +34,6 @@ public class Application implements CommandLineRunner {
     public static void main(String args[]) {
         System.out.println("SpringApplication run BEGIN ****** ");
 
-        try {
-            initMyBatis();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         SpringApplication.run(Application.class);
 
         System.out.println("SpringApplication run OK !!!!!! ");
@@ -57,25 +43,6 @@ public class Application implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("name:" + myConfig.getName());
         System.out.println("phone:" + myConfig.getPhone());
-
-        //initMyBatis();
-    }
-
-    private static void  initMyBatis() throws IOException {
-        String resource = "mybatis-config.xml";
-        Reader reader = Resources.getResourceAsReader(resource);
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(reader);
-
-        SqlSession session = factory.openSession();
-        try {
-            CityMapper cityMapper = session
-                    .getMapper(CityMapper.class);
-            City city = cityMapper.selectByCityId(1);
-            System.out.println("city: " + city.getName());
-        } finally {
-            session.close();
-        }
     }
 }
 
--- src/main/java/com/example/controller/CityController.java
+++ src/main/java/com/example/controller/CityController.java
@@ -1,8 +1,17 @@
 package com.example.controller;
 
+import com.example.datasource.dao.City;
+import com.example.datasource.mapper.CityMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.Reader;
 
 /**
  * Created by sutaiyun on 2017/3/13.
@@ -13,6 +22,28 @@ public class CityController {
 
     @RequestMapping(value = {"", "/"} )
     public String getAllCities() {
-        return "beijing, shanghai";
+        try {
+            return getCite();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Error!!!!!!!!!";
+    }
+
+    private String  getCite() throws IOException {
+        String resource = "mybatis-config.xml";
+        Reader reader = Resources.getResourceAsReader(resource);
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(reader);
+
+        SqlSession session = factory.openSession();
+        try {
+            CityMapper cityMapper = session
+                    .getMapper(CityMapper.class);
+            City city = cityMapper.selectByCityId(1);
+            return "city: " + city.getName();
+        } finally {
+            session.close();
+        }
     }
 }
