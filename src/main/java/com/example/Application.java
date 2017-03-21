ed025fc56ea03790bae1b9b314d2f090ec983a0b
sutaiyun <sutaiyun@longcheer.net>
2017-03-21 13:47:13 +0800

    增加kafka模块。
    注意 更新了  spring boot的版本。 （否则 和 kafka版本不匹配， 会出现Class not found 错误）
    同时更新了  mybatis-spring版本（更新为较新版本）

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
